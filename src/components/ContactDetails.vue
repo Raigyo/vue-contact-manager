94382ff559d92a61a29d089295298796ed7fd631
chilot <https://github.com/Raigyo>
2020-04-15 22:34:33 +0200

    update/delete item wip

--- src/components/ContactDetails.vue
+++ src/components/ContactDetails.vue
@@ -1,7 +1,8 @@
 <template>
 <div>
-  <!-- Display-->
-  <div class='card' style='width: 18rem;'>
+  <!-- Conditional rendering -->
+  <!-- Display we use props from contact object-->
+  <div v-if="!isInEditMode" class='card' style='width: 18rem;'>
     <div class='card-body'>
       <h5 class='card-title'>
         {{ contact.data().firstName }} {{ contact.data().lastName.toUpperCase() }}
@@ -12,10 +13,10 @@
       <button class="btn btn-sm btn-danger" @click="deleteContact">Delete</button>
     </div>
   </div>
-  <!-- Edit-->
+  <!-- Edit we use props from contact object-->
   <div v-if="isInEditMode" class="card" style="width: 18rem;margin-bottom: 10px;">
     <div class="card-body">
-      <!-- we clone and work on a copy of the contact -->
+      <!-- we clone and work on a copy of the contact (shallow copy)-->
       <input type="text" v-model="contactCopy.firstName"><br>
       <input type="text" v-model="contactCopy.lastName"><br>
       <input type="text" v-model="contactCopy.email"><br>
@@ -23,7 +24,7 @@
 
       <h5 class="card-title">{{contact.data().lastName}} {{contact.data().firstName}}</h5>
       <h6 class="card-subtitle mb-2 text-muted">{{contact.data().email}}</h6>
-      <p class="card-text">numéro de téléphone : {{contact.data().phone}}</p>
+      <p class="card-text">Numéro de téléphone : {{contact.data().phone}}</p>
       <button
         class="btn btn-sm btn-success"
         @click="saveEdit">
@@ -45,21 +46,40 @@ export default {
   data() {
     return {
       contactCopy: {},
+      isInEditMode: false,
     };
   },
   methods: {
     editContact() {
-
-    },
-  },
+      // ES6 'destructuring': avoid to repeat assignment for each variable
+      // 'property shorthand -  ex: firstName: firstName => firstName
+      /* eslint-disable object-curly-newline */
+      const { firstName, lastName, phone, email } = this.contact.data();
+      /* eslint-enable object-curly-newline */
+      this.contactCopy = {
+        id: this.contact.id,
+        firstName,
+        lastName,
+        phone,
+        email,
+      };
+      this.isInEditMode = true;
+    }, // \editContact
+    deleteContact() {
+    }, // \deleteContact
+    saveEdit() {
+    }, // \saveEdit
+    cancelEdit() {
+    }, // \cancelEdit
+  }, // \methods
 };
 </script>
 
 <style scoped>
 .card {
-        margin: 0 auto; /* Added */
-        float: none; /* Added */
-        margin-bottom: 10px; /* Added */
+  margin: 0 auto;
+  float: none;
+  margin-bottom: 10px;
 }
 button {
   margin: 0px 5px;
